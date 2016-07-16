558e28bd7340f09574ea6032aaf410dd98c71037
Jorge Timón <jtimon@jtimon.cc>
2016-07-16 22:28:37 +0200

    Consensus: Decouple Consensus::CheckTxInputsScripts from STANDARD_NOT_MANDATORY_VERIFY_FLAGS

--- src/main.cpp
+++ src/main.cpp
@@ -1293,6 +1293,25 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState& state, const C
         // Check against previous transactions
         // This is done last to help prevent CPU exhaustion denial-of-service attacks.
         if (!Consensus::CheckTxInputsScripts(tx, state, view, scriptVerifyFlags, true)) {
+            // Check whether the failure was caused by a
+            // non-mandatory script verification check, such as
+            // non-standard DER encodings or non-null dummy
+            // arguments; if so, don't trigger DoS protection to
+            // avoid splitting the network between upgraded and
+            // non-upgraded nodes.
+            CValidationState stateAux;
+            if (scriptVerifyFlags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS &&
+                !Consensus::CheckTxInputsScripts(tx, stateAux, view, scriptVerifyFlags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, true))
+                state.Invalid(false, REJECT_NONSTANDARD, stateAux.GetRejectReason());
+
+            // Failures of other flags indicate a transaction that is
+            // invalid in new blocks, e.g. a invalid P2SH. We DoS ban
+            // such nodes as they are not following the protocol. That
+            // said during an upgrade careful thought should be taken
+            // as to the correct behavior - we may want to continue
+            // peering with non-upgraded nodes even after soft-fork
+            // super-majority signaling has occurred.
+
             // SCRIPT_VERIFY_CLEANSTACK requires SCRIPT_VERIFY_WITNESS, so we
             // need to turn both off, and compare against just turning off CLEANSTACK
             // to see if the failure is specifically due to witness validation.
@@ -1766,24 +1785,7 @@ bool Consensus::CheckTxInputsScripts(const CTransaction& tx, CValidationState& s
         ScriptError error;
 
         if (!VerifyScript(scriptSig, scriptPubKey, witness, flags, checker, &error)) {
-                  if (flags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS) {
-                        // Check whether the failure was caused by a
-                        // non-mandatory script verification check, such as
-                        // non-standard DER encodings or non-null dummy
-                        // arguments; if so, don't trigger DoS protection to
-                        // avoid splitting the network between upgraded and
-                        // non-upgraded nodes.
-                        if (VerifyScript(scriptSig, scriptPubKey, witness, flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, checker, &error))
-                            return state.Invalid(false, REJECT_NONSTANDARD, strprintf("non-mandatory-script-verify-flag (%s)", ScriptErrorString(error)));
-                    }
-                    // Failures of other flags indicate a transaction that is
-                    // invalid in new blocks, e.g. a invalid P2SH. We DoS ban
-                    // such nodes as they are not following the protocol. That
-                    // said during an upgrade careful thought should be taken
-                    // as to the correct behavior - we may want to continue
-                    // peering with non-upgraded nodes even after soft-fork
-                    // super-majority signaling has occurred.
-                    return state.DoS(100,false, REJECT_INVALID, strprintf("mandatory-script-verify-flag-failed (%s)", ScriptErrorString(error)));
+            return state.DoS(100, false, REJECT_INVALID, strprintf("script-verify-failed (%s)", ScriptErrorString(error)));
         }
     }
 
