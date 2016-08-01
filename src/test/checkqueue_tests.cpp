dddf7ff929cfe8b7ee00a89c0a17e5168878767b
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2016-08-01 16:33:26 -0400

    add speed test

--- src/test/checkqueue_tests.cpp
+++ src/test/checkqueue_tests.cpp
@@ -34,25 +34,48 @@ struct Dummy {
 
     void swap(Dummy& x){};
 };
-CCheckQueue<Dummy, (size_t)1000000, 16> queue;
+struct Dummy2 {
+    bool operator()(){
+        return true;
+    }
+    bool operator()(std::function<void()>& z){
+        return true;
+    }
+    void swap(Dummy2& x){};
+};
+CCheckQueue<Dummy, (size_t)100000, 16> big_queue;
+CCheckQueue<Dummy, (size_t)100, 16> small_queue;
+CCheckQueue<Dummy2, (size_t)100000, 16> fast_queue;
 void reset() {
-    queue.reset_quit_queue();
-    queue.reset_ids();
-    queue.reset_masterMayEnter();
-    queue.reset_jobs();
+    big_queue.reset_quit_queue();
+    big_queue.reset_ids();
+    big_queue.reset_masterMayEnter();
+    big_queue.reset_jobs();
+
+    small_queue.reset_quit_queue();
+    small_queue.reset_ids();
+    small_queue.reset_masterMayEnter();
+    small_queue.reset_jobs();
+
+
+    fast_queue.reset_quit_queue();
+    fast_queue.reset_ids();
+    fast_queue.reset_masterMayEnter();
+    fast_queue.reset_jobs();
 };
 
 BOOST_AUTO_TEST_CASE(test_CheckQueue_PriorityWorkQueue)
 {
 
     fPrintToConsole = true;
-    CCheckQueue_Helpers::PriorityWorkQueue<decltype(queue)::Proto> work(0, 16);
+    CCheckQueue_Helpers::PriorityWorkQueue<decltype(big_queue)::Proto> work(0, 16);
     auto m = 0;
     work.add(100);
     BOOST_CHECK(!work.empty());
     size_t x = work.pop();
     BOOST_CHECK(x == 0);
     size_t x2 = work.pop();
+    BOOST_TEST_MESSAGE("GOT: x2 = " <<x2);
     BOOST_CHECK(x2 == 16);
     m = 2;
     while (!work.empty()) {
@@ -86,28 +109,28 @@ BOOST_AUTO_TEST_CASE(test_CheckQueue_PriorityWorkQueue)
     BOOST_CHECK(m == 1000);
 }
 
-CCheckQueue_Helpers::job_array<decltype(queue)::Proto> jobs;
+CCheckQueue_Helpers::job_array<decltype(big_queue)::Proto> jobs;
 static std::atomic<size_t> m;
 BOOST_AUTO_TEST_CASE(test_CheckQueue_job_array)
 {
     fPrintToConsole = true;
-    for (size_t i = 0; i < decltype(queue)::MAX_JOBS; ++i)
+    for (size_t i = 0; i < decltype(big_queue)::MAX_JOBS; ++i)
         jobs.reset_flag(i);
     m = 0;
     threadGroup.create_thread([]() {
-            for (size_t i = 0; i < decltype(queue)::MAX_JOBS; ++i)
+            for (size_t i = 0; i < decltype(big_queue)::MAX_JOBS; ++i)
             m += jobs.reserve(i) ? 1 : 0;
     });
 
     threadGroup.create_thread([]() {
-            for (size_t i = 0; i < decltype(queue)::MAX_JOBS; ++i)
+            for (size_t i = 0; i < decltype(big_queue)::MAX_JOBS; ++i)
             m += jobs.reserve(i) ? 1 : 0;
     });
     threadGroup.join_all();
 
-    BOOST_CHECK(m == decltype(queue)::MAX_JOBS);
+    BOOST_CHECK(m == decltype(big_queue)::MAX_JOBS);
 }
-CCheckQueue_Helpers::round_barrier<decltype(queue)::Proto> barrier;
+CCheckQueue_Helpers::round_barrier<decltype(big_queue)::Proto> barrier;
 BOOST_AUTO_TEST_CASE(test_CheckQueue_round_barrier)
 {
 
@@ -134,43 +157,79 @@ BOOST_AUTO_TEST_CASE(test_CheckQueue_quit)
     reset();
     auto nThreads = 8;
     for (auto i = 0; i < nThreads - 1; ++i)
-        threadGroup.create_thread([=]() {queue.Thread(nThreads); });
-    queue.quit_queue();
+        threadGroup.create_thread([=]() {big_queue.Thread(nThreads); });
+    big_queue.quit_queue();
     threadGroup.join_all();
 }
 
-
-BOOST_AUTO_TEST_CASE(test_CheckQueue_All)
+BOOST_AUTO_TEST_CASE(test_CheckQueue_Performance)
 {
 
     fPrintToConsole = true;
     reset();
     auto nThreads = 8;
     for (auto i = 0; i < nThreads - 1; ++i)
-        threadGroup.create_thread([=]() {queue.Thread(nThreads); });
+        threadGroup.create_thread([=]() {fast_queue.Thread(nThreads); });
 
-    for (auto i = 0; i < 100; ++i) {
+    std::vector<Dummy2> vChecks;
+    vChecks.reserve(100);
+    auto start_time = GetTimeMicros();
+    size_t ROUNDS = 1000;
+    for (size_t i = 0; i< ROUNDS; ++i) {
         size_t total = 0;
         {
             n = 0;
-            logf("Round Begin [ n was %q ] \n", n);
-            CCheckQueueControl<decltype(queue)> control(&queue, nThreads);
-            for (size_t j = 0; j < 20000; ++j) {
-                std::vector<Dummy> vChecks;
-                size_t r = GetRand(39)+1;
+            CCheckQueueControl<decltype(fast_queue)> control(&fast_queue, nThreads);
+            for (size_t j = 0; j < 101; ++j) {
+                size_t r = 30;
                 total += r;
-                vChecks.reserve(r);
+                vChecks.clear();
                 for (size_t k = 0; k < r; ++k)
+                    vChecks.push_back(Dummy2{});
+                control.Add(vChecks);
+            }
+        }
+    }
+    auto end_time = GetTimeMicros();
+    BOOST_TEST_MESSAGE("Perf Test took "<<end_time - start_time <<" microseconds for 100 rounds, " << (ROUNDS*1000000.0)/(end_time - start_time) << "rps");
+    fast_queue.quit_queue();
+    threadGroup.join_all();
+}
+
+BOOST_AUTO_TEST_CASE(test_CheckQueue_Correct)
+{
+
+    fPrintToConsole = true;
+    reset();
+    auto nThreads = 8;
+    for (auto i = 0; i < nThreads - 1; ++i)
+        threadGroup.create_thread([=]() {small_queue.Thread(nThreads); });
+
+    size_t count = 0;
+    for (size_t i = 0; i< 101; ++i) {
+        size_t total = i;
+        {
+            n = 0;
+            logf("Round Begin [ n was %q ] \n", n);
+            CCheckQueueControl<decltype(small_queue)> control(&small_queue, nThreads);
+            while (total) {
+                size_t r = GetRand(10);
+                std::vector<Dummy> vChecks;
+                vChecks.reserve(r);
+                for (size_t k = 0; k < r && total; k++) {
+                    total--;
                     vChecks.push_back(Dummy{});
+                }
                 control.Add(vChecks);
             }
         }
         logf("Round End [ n was %q ] \n", n);
-        BOOST_CHECK(n == total);
-        if (n != total)
-            break;
+        ++count;
+        if (n != i) {
+            BOOST_TEST_MESSAGE("Failure on trial " << count-1);
+        }
     }
-    queue.quit_queue();
+    small_queue.quit_queue();
     threadGroup.join_all();
 }
 
