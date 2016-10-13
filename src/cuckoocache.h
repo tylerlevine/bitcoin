8e88163acfabdbcc4b31818badff14c4ff80a6ef
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2016-10-13 13:38:39 -0400

    Fix documentation errors & add more documentation to functions of parameters and pre/post conditions

--- src/cuckoocache.h
+++ src/cuckoocache.h
@@ -49,6 +49,13 @@ public:
     /**
      * bit_packed_atomic_flags constructor creates memory to sufficiently
      * keep track of garbage collection information for size entries.
+     *
+     * @param size the number of elements to allocate space for
+     *
+     * @post bit_set, bit_unset, and bit_is_set function properly forall x. x <
+     * size
+     * @post All calls to bit_is_set (without subsequent bit_unset) will return
+     * true.
      */
     bit_packed_atomic_flags(uint32_t size)
     {
@@ -61,6 +68,12 @@ public:
 
     /** setup marks all entries and ensures that bit_packed_atomic_flags can store
      * at least size entries
+     *
+     * @param b the number of elements to allocate space for
+     * @post bit_set, bit_unset, and bit_is_set function properly forall x. x <
+     * b
+     * @post All calls to bit_is_set (without subsequent bit_unset) will return
+     * true.
      */
     inline void setup(uint32_t b)
     {
@@ -68,19 +81,34 @@ public:
         std::swap(mem, d.mem);
     }
 
-    /** set sets an entry as discardable.  */
+    /** bit_set sets an entry as discardable. 
+     *
+     * @param s the index of the entry to bit_set.
+     * @post immediately subsequent call (assuming proper external memory
+     * ordering) to bit_is_set(s) == true.
+     *
+     */
     inline void bit_set(uint32_t s)
     {
         mem[s >> 3].fetch_or(1 << (s & 7), std::memory_order_relaxed);
     }
 
-    /**  unset marks an entry as something that should not be overwritten  */
+    /**  bit_unset marks an entry as something that should not be overwritten  
+     *
+     * @param s the index of the entry to bit_unset.
+     * @post immediately subsequent call (assuming proper external memory
+     * ordering) to bit_is_set(s) == false.
+     */
     inline void bit_unset(uint32_t s)
     {
         mem[s >> 3].fetch_and(~(1 << (s & 7)), std::memory_order_relaxed);
     }
 
-    /** queries the set for discardability at n*/
+    /** bit_is_set queries the table for discardability at s
+     *
+     * @param s the index of the entry to read.
+     * @returns if the bit at index s was set.
+     * */
     inline bool bit_is_set(uint32_t s) const
     {
         return (1 << (s & 7)) & mem[s >> 3].load(std::memory_order_relaxed);
@@ -122,9 +150,9 @@ public:
  *   - The name "please_keep" is used because elements may be erased anyways on insert.
  *
  * @tparam Element should be a POD type that is 32-alignable
- * @tparam Hash should be a function/callable which takes a Element and an Offset and
- * extracts a hash from it. Should return high-entropy hashes for
- * Hash(k, 0) and Hash(k, 1).
+ * @tparam Hash should be a function/callable which takes a template parameter
+ * hash_select and an Element and extracts a hash from it. Should return
+ * high-entropy hashes for `Hash h; h<0>(e) and h<1>(e)`.
  */
 template <typename Element, typename Hash>
 class cache
@@ -153,29 +181,39 @@ private:
      * */
     const Hash hash_function;
 
-    /** convenience for not having to write out the modulus everywhere.
+    /** convenience for not having to write this out everywhere we compute a
+     * hash.
+     *
+     * @tparam hash_select the hash position
+     * @param e the element whose hash will be returned
+     * @returns a deterministic hash of type unint32_t derived from e.
      */
-    template <uint8_t n>
-    inline uint32_t compute_hash(const Element& t) const
+    template <uint8_t hash_select>
+    inline uint32_t compute_hash(const Element& e) const
     {
-        return hash_function.template operator()<n>(t) % size;
+        return hash_function.template operator()<hash_select>(e) % size;
     }
 
-    /* end returns a constexpr index that can never be inserted to */
+    /* end
+     * @returns a constexpr index that can never be inserted to */
     constexpr uint32_t invalid() const
     {
         return ~(uint32_t)1;
     }
 
     /** allow_erase marks the element at index n as discardable. Threadsafe
-     * without any concurrent insert. */
+     * without any concurrent insert.
+     * @param n the index to allow erasure of
+     */
     inline void allow_erase(uint32_t n) const
     {
         collection_flags.bit_set(n);
     }
 
     /** please_keep marks the element at index n as an entry that should be kept.
-     * Threadsafe without any concurrent insert. */
+     * Threadsafe without any concurrent insert.
+     * @param n the index to prioritize keeping
+     */
     inline void please_keep(uint32_t n) const
     {
         collection_flags.bit_unset(n);
@@ -192,6 +230,12 @@ public:
     }
 
     /** setup adjusts the container to store new_size elements and clears the cache if new_size != size
+     *
+     * @param new_size the new number of elements to store
+     * @post if new_size == size, no effect
+     * @post if new_size != size, inserted elements may not be at their correct
+     * location and all collection_flags are set. (still possible for some
+     * elements to not be evicted).
      **/
     void setup(uint32_t new_size)
     {
@@ -205,10 +249,13 @@ public:
         depth_limit = std::max((uint8_t)1, static_cast<uint8_t>(std::log2(static_cast<float>(size))));
     }
 
-    /** setup bytes is a convenience function which accounts for internal
+    /** setup_bytes is a convenience function which accounts for internal
      * memory usage when deciding how many elements to store. It isn't perfect
      * because it doesn't account for MallocUsage of collection_flags or table.
-     * This difference is small/0, so we ignore it.
+     * This difference is small or 0, so we ignore it.
+     *
+     * @param bytes the approximate number of bytes to use for this data
+     * structure.
      */
     void setup_bytes(size_t bytes)
     {
@@ -225,22 +272,34 @@ public:
      * It drops the last tried element if it runs out of depth before
      * encountering an open slot.
      *
+     * Thus
+     *
+     * insert(x);
+     * return contains(x, false);
+     *
+     * is not guaranteed to return true.
+     *
+     * @param e the element to insert
+     * @post one of the following: All previously inserted elements and e are
+     * now in the table, one previously inserted element is evicted from the
+     * table, the entry attempted to be inserted is evicted.
+     *
      */
-    inline void insert(Element k)
+    inline void insert(Element e)
     {
         uint32_t last_loc = invalid();
-        uint32_t locs[2] = {compute_hash<0>(k), compute_hash<1>(k)};
+        uint32_t locs[2] = {compute_hash<0>(e), compute_hash<1>(e)};
         // Make sure we have not already inserted this element
         // If we have, make sure that it does not get deleted
         for (uint32_t loc : locs)
-            if (table[loc] == k)
+            if (table[loc] == e)
                 return please_keep(loc);
         for (uint8_t depth = 0; depth < depth_limit; ++depth) {
             // First try to insert to an empty slot, if one exists
             for (uint32_t loc : locs) {
                 if (!collection_flags.bit_is_set(loc))
                     continue;
-                table[loc] = std::move(k);
+                table[loc] = std::move(e);
                 please_keep(loc);
                 return;
             }
@@ -256,11 +315,11 @@ public:
             * for the next iteration.
             */
             last_loc = last_loc == locs[0] ? locs[1] : locs[0];
-            std::swap(table[last_loc], k);
+            std::swap(table[last_loc], e);
 
             // Recompute the locs -- unfortunately happens one too many times!
-            locs[0] = compute_hash<0>(k);
-            locs[1] = compute_hash<1>(k);
+            locs[0] = compute_hash<0>(e);
+            locs[1] = compute_hash<1>(e);
         }
     }
 
@@ -271,28 +330,36 @@ public:
      * garbage is only collected when the space is needed), so:
      *
      * insert(x);
-     * contains(x, true);
-     * return contains(x, false);
+     * if (contains(x, true))
+     *     return contains(x, false);
+     * else
+     *     return true;
      *
      * executed on a single thread will always return true!
      *
      * This is a great property for re-org performance for example.
      *
      * contains returns a bool set true if the element was found.
+     *
+     * @param e the element to check
+     * @param erase
+     *
+     * @post if erase is true and the element is found, then the garbage collect
+     * flag is set
+     * @returns true if the element is found, false otherwise
      */
-
-    inline bool contains(const Element& k, const bool erase) const
+    inline bool contains(const Element& e, const bool erase) const
     {
         if (erase) {
-            uint32_t locs[2] = {compute_hash<0>(k), compute_hash<1>(k)};
+            uint32_t locs[2] = {compute_hash<0>(e), compute_hash<1>(e)};
             for (uint32_t loc : locs)
-                if (table[loc] == k) {
+                if (table[loc] == e) {
                     allow_erase(loc);
                     return true;
                 }
             return false;
         } else
-            return table[compute_hash<0>(k)] == k || table[compute_hash<1>(k)] == k;
+            return table[compute_hash<0>(e)] == e || table[compute_hash<1>(e)] == e;
     }
 };
 }
