59cb5910af07eb96c2dd4282e22085ead04a0b35
Jeremy Rubin <j@rubin.io>
2019-05-14 11:45:21 -0700

    Simplify classic script verification logic

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1626,19 +1626,20 @@ bool VerifyScript(const CScript& scriptSig, const CScript& scriptPubKey, const C
         // Perform Classic Script Verification
     }
 
-    std::vector<std::vector<unsigned char> > stack, stackCopy;
-    if (!EvalScript(stack, scriptSig, flags, checker, SigVersion::BASE, serror))
-        // serror is set
-        return false;
-    if (flags & SCRIPT_VERIFY_P2SH)
-        stackCopy = stack;
-    if (!EvalScript(stack, scriptPubKey, flags, checker, SigVersion::BASE, serror))
-        // serror is set
-        return false;
-    if (stack.empty())
-        return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
-    if (CastToBool(stack.back()) == false)
-        return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
+    // Classic Script Verification
+    std::vector<std::vector<unsigned char> > stack;
+    {
+        if (!EvalScript(stack, scriptSig, flags, checker, SigVersion::BASE, serror))
+            // serror is set
+            return false;
+        if (!EvalScript(stack, scriptPubKey, flags, checker, SigVersion::BASE, serror))
+            // serror is set
+            return false;
+        if (stack.empty())
+            return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
+        if (CastToBool(stack.back()) == false)
+            return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
+    }
 
 
     // The CLEANSTACK check is only performed after potential P2SH evaluation,
