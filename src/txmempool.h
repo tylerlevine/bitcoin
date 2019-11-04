ada18cd8977f04b0a1a10af82ef9e816ed0b95f4
Jeremy Rubin <j@rubin.io>
2019-11-04 14:52:28 -0800

    Add epoch fields to Mempool

--- src/txmempool.h
+++ src/txmempool.h
@@ -131,6 +131,14 @@ public:
     int64_t GetSigOpCostWithAncestors() const { return nSigOpCostWithAncestors; }
 
     mutable size_t vTxHashesIdx; //!< Index in mempool's vTxHashes
+private:
+    mutable uint64_t m_epoch; //!< epoch when last touched, useful for graph algorithms
+public:
+    bool already_touched(uint64_t during) const {
+        bool ret = m_epoch >= during;
+        m_epoch = std::max(m_epoch, during);
+        return ret;
+    }
 };
 
 // Helpers for modifying CTxMemPool::mapTx, which is a boost multi_index.
@@ -455,6 +463,7 @@ private:
     mutable int64_t lastRollingFeeUpdate;
     mutable bool blockSinceLastRollingFeeBump;
     mutable double rollingMinimumFeeRate; //!< minimum fee to get into the pool, decreases exponentially
+    mutable uint64_t m_epoch;
 
     void trackPackageRemoved(const CFeeRate& rate) EXCLUSIVE_LOCKS_REQUIRED(cs);
 
@@ -738,6 +747,9 @@ private:
      *  removal.
      */
     void removeUnchecked(txiter entry, MemPoolRemovalReason reason) EXCLUSIVE_LOCKS_REQUIRED(cs);
+public:
+    // This function mutates mutable state!
+    uint64_t GetFreshEpoch() const EXCLUSIVE_LOCKS_REQUIRED(cs);
 };
 
 /**
