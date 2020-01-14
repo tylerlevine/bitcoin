3023a16682a5b778b7c8b5b98ef166d2bcea2bd2
Jeremy Rubin <j@rubin.io>
2020-01-14 04:14:26 -0800

    Simplified & Corrected UpdateForDescendants. Fixes a bug in dealing with child & grandchild overlap

--- src/txmempool.h
+++ src/txmempool.h
@@ -536,7 +536,7 @@ public:
     const setEntries & GetMemPoolChildren(txiter entry) const EXCLUSIVE_LOCKS_REQUIRED(cs);
     uint64_t CalculateDescendantMaximum(txiter entry) const EXCLUSIVE_LOCKS_REQUIRED(cs);
 private:
-    typedef std::map<txiter, setEntries, CompareIteratorByHash> cacheMap;
+    typedef std::map<txiter, std::vector<txiter>, CompareIteratorByHash> cacheMap;
 
     struct TxLinks {
         setEntries parents;
