9568d182b2c3ec09633d1b81a644437860d790b5
Tyler Levine <tyler@bitgo.com>
2020-02-01 00:31:03 -0800

    Run feature_ctv_vault.py functional test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -112,6 +112,7 @@ BASE_SCRIPTS = [
     'wallet_keypool_topup.py',
     'feature_fee_estimation.py',
     'interface_zmq.py',
+    'feature_ctv_vault.py',
     'interface_bitcoin_cli.py',
     'mempool_resurrect.py',
     'wallet_txn_doublespend.py --mineblock',
